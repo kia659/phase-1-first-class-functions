0ce7f20b414e0528664ecf572e8f418f3765e005
Kia Neitzell <kia.neitzell@gmail.com>
2024-01-30 14:52:51 -0800

    compleated

---
+++ index.js
@@ -0,0 +1,18 @@
+function spy(){
+return 5
+}
+function receivesAFunction(spy) {
+spy()
+}
+function returnsANamedFunction(){
+
+return function name(){
+
+}
+}
+function returnsAnAnonymousFunction(){
+
+return function (){
+    
+}
+}
